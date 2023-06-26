175fe8993240db70d7dfc0dd1be26fada780b669
dark_dip2 <diptya99@gmail.com>
2023-06-27 01:13:48 +0530

    dbconfig

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   DB_NAME: "crm_db",
-  DB_URL: "mongodb://localhost/crm-db",
+  DB_URL: "mongodb+srv://diptya99:Password67@cluster0.z4i3cww.mongodb.net/"
+  //"mongodb://localhost/crm-db",
 };
